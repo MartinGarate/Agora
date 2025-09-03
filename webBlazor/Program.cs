6ba71afe431a43693c631d8a375c25b1dffa089d
valeinfo <106827085+Valeinforma@users.noreply.github.com>
2025-09-03 18:47:57 -0300

    Añadir autenticación con Firebase en la aplicación Blazor
    Se ha implementado un sistema de autenticación utilizando Firebase en la aplicación Blazor.
    El archivo `Login.razor` incluye componentes para el inicio de sesión y registro de usuarios,
    con campos para correo electrónico y contraseña, y lógica para manejar la autenticación y el cierre de sesión
    a través del servicio `FirebaseAuthService`.
    
    Se ha registrado `FirebaseAuthService` en `Program.cs` y se ha añadido el paquete
    `CurrieTechnologies.Razor.SweetAlert2` para mostrar alertas personalizadas.
    
    Además, se han actualizado `_Imports.razor` y `webBlazor.csproj` para incluir las nuevas dependencias.
    En `index.html`, se han añadido los scripts necesarios para la configuración de Firebase y
    la gestión de la autenticación.
    
    Por último, se ha creado el servicio `FirebaseAuthService` que utiliza `IJSRuntime`
    para interactuar con las funciones de JavaScript de Firebase, gestionando el inicio de sesión,
    la creación de usuarios y el almacenamiento del ID del usuario en el almacenamiento local.

--- webBlazor/Program.cs
+++ webBlazor/Program.cs
@@ -1,8 +1,10 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Service.Interfaces;
 using Service.Services;
 using webBlazor;
+using webBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
-
+builder.Services.AddScoped<FirebaseAuthService>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
